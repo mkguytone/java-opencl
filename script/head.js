dfda4b5c572e8cf8a30b476f51aa9fd5ef6b2702
mkguytone <m.komatsu.05@cantab.net>
2020-02-27 21:49:41 +0900

    script

--- script/head.js
+++ script/head.js
@@ -6,10 +6,10 @@ function gtag(){
 gtag('js', new Date());
 gtag('config', 'UA-143005445-1');
 
-
+/*
 document.write('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
 (adsbygoogle = window.adsbygoogle || []).push({
   google_ad_client: "ca-pub-6982397509579717",
   enable_page_level_ads: true
 });
-
+*/
